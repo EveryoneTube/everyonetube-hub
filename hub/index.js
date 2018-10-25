989ab4deec495937f19dc679dfc54bed781685de
nemanjan00 <nemanjan00@gmail.com>
2018-10-25 20:11:01 +0200

    cleanup

--- hub/index.js
+++ hub/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
 const app = express();
 
 const config = require("./config").getConfig();
+
+app.use("/", require("./routes"));
 
 app.listen(config.port, () => {
 	console.log("EveryoneYoutube Hub is listening on port " + config.port);
