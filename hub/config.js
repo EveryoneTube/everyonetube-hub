91cc5d697f245a6e51d37fd734fbd34d57a27978
nemanjan00 <nemanjan00@gmail.com>
2018-10-25 20:09:40 +0200

    Proxy config

--- hub/config.js
+++ hub/config.js
@@ -1,7 +1,7 @@
 // Config singelton
 
 const config = {
-	proxy: process.env.PROXY || "http://localhost:9050/",
+	proxy: process.env.PROXY || "socks://localhost:9050/",
 	port: process.env.PORT || 8080
 };
 
